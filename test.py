dc5312b2a2d5feee9ad0d858b3ecf92396b15806
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2022-09-05 12:27:47 +0530

    new program

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
-l = []
-n = int(input())
-k = int(input())
-print(n,k)
-if k%2==0:
-  for i in range(n):
-    if i == 0 or i%2==0:
-      l.append(1)
-    else:
-      l.append(0)
-else:
-  for i in range(n):
-    if i == 0 or i%2==0:
-      l.append(0)
-    else:
-      l.append(1)
-print(l,end=" ")
+inputList = list(map(int,input().split()))
+dict = {}
+
+for i in inputList:
+  dict[i] = []
+
+i=0
+for j in inputList:
+  dict[j].append(i)
+  i+=1
+
+res_dict={}
+
+for key,val in dict.items():
+  if len(val) >= 2:
+    res_dict[key]=val
+
+print(res_dict,end="")
