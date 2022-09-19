cc9400a0571705a12b5234f4b4c2f52b3aada6f6
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2022-09-19 09:18:00 +0530

    removed test file

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-inputList = list(map(int,input().split()))
-dict = {}
-
-for i in inputList:
-  dict[i] = []
-
-i=0
-for j in inputList:
-  dict[j].append(i)
-  i+=1
-
-res_dict={}
-
-for key,val in dict.items():
-  if len(val) >= 2:
-    res_dict[key]=val
-
-print(res_dict,end="")
+a = 2
+match(a):
+  case a:
+    print(f"hi {a}")
--- test3.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-for i in range(1,n*2+1):
-  if i<=n:
-    print('* '*i,end="\n")
-  elif i>n:
-    print('* '*(n*2-i),end="\n")
