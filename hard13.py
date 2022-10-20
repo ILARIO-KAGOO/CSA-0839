c3be6b1a38a5f7dd26a69e9551c57477a5abeadc
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2022-10-20 13:21:30 +0530

    day 2 revision

---
+++ hard13.py
@@ -0,0 +1,13 @@
+a = list(map(int,input().split()))
+left = 0
+right = len(a)-1
+area = 0
+
+while left<right:
+  area = max(area,min(a[left],a[right])*(right-left))
+  if a[left]<a[right]:
+    left+=1
+  else:
+    right-=1
+
+print(f"The largest area is {area}")
---
+++ hard17.py
@@ -0,0 +1,4 @@
+s = input("Enter first binary number: ")
+d = input("Enter second binary number: ")
+sumi = lambda a,b : bin(int(a, 2) + int(b, 2))
+print(sumi(s,d)[2:])
